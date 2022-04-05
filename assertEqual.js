394e1ad6203af703b4c3d7929d380110c6823755
Kelly Atmore <kelly.atmore47@gmail.com>
2022-04-04 17:03:47 -0700

    assertion code

---
+++ assertEqual.js
@@ -0,0 +1,14 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("🙂🙂🙂Assertion Passed: [" + actual + "] === [" + expected + "]");
+  } else {
+    console.log("☠️ ☠️ ☠️ Assertion Failed: [" + actual + "] !== [" + expected + "]");
+  }
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("hello", "hello");
+assertEqual(1, 6);
