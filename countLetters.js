20e90296701de4813d68cad4f2dd1dd66e5ed84b
Kelly Atmore <kelly.atmore47@gmail.com>
2022-04-06 15:57:00 -0700

    count letters function

---
+++ countLetters.js
@@ -0,0 +1,25 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`🙂🙂🙂Assertion Passed: [${actual}] === [${expected}]`);
+  } else {
+    console.log(`☠️ ☠️ ☠️ Assertion Failed: [${actual}] !== [${expected}]`);
+  }
+};
+
+const countletters = function(str) {
+
+  let obj = {};
+  let arr = str.toLowerCase().replace(/\s/g, '').split('');
+  console.log(arr);
+  
+  for (const letter of arr) {
+    if (obj[letter]) {
+      obj[letter] += 1;
+    } else {
+      obj[letter] = 1;
+    }
+  }
+  return obj;
+};
+
+console.log(countletters("Hello World"));
