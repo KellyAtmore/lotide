735795969d780fb0de20653964cfa411ad518e6d
Kelly Atmore <kelly.atmore47@gmail.com>
2022-04-05 19:48:39 -0700

    assert arrays function

---
+++ assertArraysEqual.js
@@ -0,0 +1,31 @@
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  } else {
+    let result = false;
+    
+    // comparing each element of array
+    for (let i = 0; i < array1.length; i++) {
+
+      if (array1[i] !== array2[i]) {
+        return false;
+      } else {
+        result = true;
+      }
+    }
+    return result;
+  }
+};
+
+const assertArraysEqual = function(array1, array2) {
+  if (eqArrays(array1, array2)) {
+    console.log(`🙂🙂🙂Assertion Passed: [${array1}] === [${array2}]`);
+} else {
+    console.log(`🛑🛑🛑 Assertion Failed: [${array1}] !== [${array2}]`);
+}
+};
+
+assertArraysEqual([1, 2, 3], [1, 2, 3, 4]);
+assertArraysEqual([1, 2, 3], [3, 2, 3]);
+assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]);
+assertArraysEqual(["1", "2", "3"], ["1", "2", 3]);
