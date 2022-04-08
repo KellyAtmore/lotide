8d88a79d3af72f435ebebddfebf054fa253aa704
Kelly Atmore <kelly.atmore47@gmail.com>
2022-04-07 17:14:26 -0700

    map function

---
+++ map.js
@@ -0,0 +1,60 @@
+const words = ["ground", "control", "to", "major", "tom"];
+
+const map = function(array, callback) {
+  const results = [];
+  for (let item of array) {
+    results.push(callback(item));
+  }
+  return results;
+};
+
+
+const results1 = map(words, word => word[0]);
+console.log(results1);
+
+
+
+//FOR TESTING
+
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  } else {
+    let result = false;
+    
+    // comparing each element of array
+    for (let i = 0; i < array1.length; i++) {
+
+      if (array1[i] !== array2[i]) {
+        return false;
+      } else {
+        result = true;
+      }
+    }
+    return result;
+  }
+};
+
+
+const assertArraysEqual = function(array1, array2) {
+  if (eqArrays(array1, array2)) {
+    console.log(`🙂🙂🙂Assertion Passed: [${array1}] === [${array2}]`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: [${array1}] !== [${array2}]`);
+  }
+};
+
+
+//TESTING
+
+const testArray1 = ['bacon', 'gravy', 'soda'];
+const testArray2 = [1, 2, 3];
+const testArray3 = [3, 2, 1];
+
+
+assertArraysEqual(map(testArray1, word => word[0]), ['b', 'g', 's']);
+assertArraysEqual(map(testArray2, num => num * 2), [2, 4, 6]);
+assertArraysEqual(map(testArray3, num => num > 2), [true, false, false]);
+
+
+
