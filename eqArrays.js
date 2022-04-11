a534d320311674b4082417af7d23b17a693a3b66
Kelly Atmore <kelly.atmore47@gmail.com>
2022-04-11 14:52:53 -0700

    refactored with modules

--- eqArrays.js
+++ eqArrays.js
@@ -1,12 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`🙂🙂🙂Assertion Passed: [${actual}] === [${expected}]`);
-  } else {
-    console.log(`☠️ ☠️ ☠️ Assertion Failed: [${actual}] !== [${expected}]`);
-  }
-};
-
-
 
 const eqArrays = function(array1, array2) {
   if (array1.length !== array2.length) {
@@ -23,6 +14,6 @@ const eqArrays = function(array1, array2) {
   return true;
 };
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays([1, 3], [1, 2, 3]), false);
-assertEqual(eqArrays(["hello", "goodbye"], ["hello", "goodbye"]), true);
+module.exports = eqArrays;
+
+
